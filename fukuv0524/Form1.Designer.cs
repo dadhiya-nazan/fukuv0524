856c838a9a9de7b04fc3adf25736abe63485787e
Oshima Shuya <104044123+oshima27dat@users.noreply.github.com>
2024-05-24 13:25:46 +0900

    ラベル　ボタンを作る　動かす

--- fukuv0524/Form1.Designer.cs
+++ fukuv0524/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Yu Gothic UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(360, 253);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 28);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(656, 312);
+            button1.Name = "button1";
+            button1.Size = new Size(58, 58);
+            button1.TabIndex = 1;
+            button1.Text = "右";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Yu Gothic UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(592, 253);
+            button3.Name = "button3";
+            button3.Size = new Size(58, 58);
+            button3.TabIndex = 3;
+            button3.Text = "上";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Yu Gothic UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(528, 312);
+            button4.Name = "button4";
+            button4.Size = new Size(58, 58);
+            button4.TabIndex = 4;
+            button4.Text = "左";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Yu Gothic UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(592, 376);
+            button5.Name = "button5";
+            button5.Size = new Size(58, 58);
+            button5.TabIndex = 5;
+            button5.Text = "下";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
