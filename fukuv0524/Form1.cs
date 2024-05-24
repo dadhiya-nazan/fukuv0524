3ccbdddc23577ca8bd93c6953227a44c081c7794
Oshima Shuya <104044123+oshima27dat@users.noreply.github.com>
2024-05-24 13:33:11 +0900

    おまけ一部

--- fukuv0524/Form1.cs
+++ fukuv0524/Form1.cs
@@ -10,21 +10,25 @@ namespace fukuv0524
         private void button4_Click(object sender, EventArgs e)
         {
             label1.Left = label1.Left - 20;
+            label1.Text = "左に行くよ";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Left = label1.Left + 20;
+            label1.Text = "右に行くよ";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             label1.Top = label1.Top - 20;
+            label1.Text = "上に行くよ";
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             label1.Top = label1.Top + 20;
+            label1.Text = "下に行くよ";
         }
 
         private void label1_Click(object sender, EventArgs e)
